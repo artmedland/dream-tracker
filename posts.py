cf44447433f288f1b259f35639fe09431baad43c
puuhapake <artmedland@gmail.com>
2026-03-26 20:32:09 +0200

    SOC post retrieval to posts module

---
+++ posts.py
@@ -0,0 +1,34 @@
+import db
+
+def add(user_id, title, quality, dream):
+    db.execute("""
+        INSERT INTO Posts (poster_id, title, sleep_quality, dream)
+        VALUES (?, ?, ?, ?)
+    """, [user_id, title, quality, dream])
+
+def get(uid=None):
+    if uid is None:
+        query = """
+            SELECT p.id, p.title, u.username
+            FROM Posts p, Users u
+            WHERE p.poster_id = u.id
+            ORDER BY p.id DESC"""
+        return db.query(query)
+    if isinstance(uid, int):
+        query = """
+            SELECT p.title, u.username,
+                   p.sleep_quality, p.dream
+            FROM Posts p, Users u
+            WHERE p.poster_id = u.id
+              AND p.id = ?"""
+        return db.query(query, [uid])[0]
+    else:
+        raise NotImplementedError
+
+def user_count():
+    query = "SELECT COUNT(id) count FROM Users"
+    return db.query(query)[0]["count"]
+
+def post_count():
+    query = "SELECT COUNT(id) count FROM Posts"
+    return db.query(query)[0]["count"]
