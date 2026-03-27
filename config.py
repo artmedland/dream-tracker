21400bc673959ddd19928debd48ffc1048237fa3
puuhapake <artmedland@gmail.com>
2026-03-27 15:58:12 +0200

    Add automatic config dir creation

--- config.py
+++ config.py
@@ -4,13 +4,16 @@ from pathlib import Path
 session_key = None
 
 WORKING_DIR = Path(__file__).parent
-KEY_PATH = WORKING_DIR / "config" / ".secret_key"
+CONFIG_PATH = WORKING_DIR / "config"
+KEY_PATH = CONFIG_PATH / ".secret_key"
 
 def get_session_key():
     """Retrieves a secret session key file if it exists, 
     or creates a new one otherwise.
     """
     global session_key
+    CONFIG_PATH.mkdir(parents=True, exist_ok=True)
+
     if KEY_PATH.exists():
         session_key = KEY_PATH.read_text()
         return session_key
