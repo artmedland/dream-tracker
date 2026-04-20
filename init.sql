68c8c6c319763bb709ad69a910881d8075db61cf
puuhapake <artmedland@gmail.com>
2026-04-20 20:21:01 +0300

    Add indexing to database

--- init.sql
+++ init.sql
@@ -1,3 +1,13 @@
+CREATE INDEX IF NOT EXISTS p_vis_id ON Posts(visibility, id DESC);
+CREATE INDEX IF NOT EXISTS p_vis_uid ON Posts(user_id, visibility);
+
+CREATE INDEX IF NOT EXISTS l_likes ON Likes(post_id);
+
+CREATE INDEX IF NOT EXISTS f_friend ON Friends(user_id, friend_id);
+
+CREATE INDEX IF NOT EXISTS t_tag ON Tags(post_id, tag);
+CREATE INDEX IF NOT EXISTS cat_post ON PostCategories(post_id, category, choice);
+
 DELETE FROM Categories;
 
 INSERT INTO Categories (category, choice) 
